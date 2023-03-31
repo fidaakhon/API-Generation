27b41867f35252399028d8a5eab3436ed70f90bb
Fida ur Rahman <108273264+fidaakhon@users.noreply.github.com>
2023-03-30 21:59:00 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+
+  const   users = [
+        {
+            "id": 1,
+            "name": "John",
+            "age": 28
+        },
+        {
+            "id": 2,
+            "name": "Jane",
+            "age": 35
+        },
+        {
+            "id": 3,
+            "name": "Bob",
+            "age": 42
+        },
+        {
+            "id": 4,
+            "name": "Alice",
+            "age": 19
+        }
+    ]
+
+    module.exports = users
